0c604934d56f69fb1e0c0c3a84ae2d8eeac94639
rembrandtcosta <rembrandt.costa@ccc.ufcg.edu.br>
2023-08-21 18:21:44 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import quotes from './quotes.json';
 
 function App() {
+  const [quote, setQuote] = useState(1);
+  const [author, setAuthor] = useState('1');
+  const [index, setIndex] = useState(0);
+  const cntQuotes = quotes.length;
+
+  useEffect(() => {
+    updateRandomIndex();
+  }, []);
+
+  useEffect(() => {
+    setQuote(JSON.stringify(quotes[index].quote));
+    setAuthor(quotes[index].author); 
+  }, [index]);
+
+  const updateRandomIndex = () => {
+    setIndex(Math.floor(Math.random() * (cntQuotes-1)));
+  }
+
+  const getTwitterURL = () => {
+    return "https://twitter.com/intent/tweet?text=" + quote + " --" + author;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div id="quote-box" className="container-sm">
+        <div className="col-12 p-0 min-vh-100 text-center m-0 d-flex flex-column justify-content-center">
+          <div>
+            <div id="center-box" className="p-0 text-center m-0">
+            <p className="text-break">
+              {quote}
+            </p>
+            </div>
+            
+            <div id="author">
+              {"-" + author}
+            </div>
+          </div>
+          <div className='row'> 
+            <div className='col-sm'/>
+            <div className='col-sm'>
+              <button 
+                id="new-quote"
+                className='btn btn-primary' 
+                onClick={updateRandomIndex}
+              >
+                New Quote
+              </button>
+            </div>
+            <div className='col-sm'>
+              <a href={getTwitterURL()} id="tweet-quote">Tweet quote</a>
+            </div>
+            <div className='col-sm'/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ tool/parse.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+const args = process.argv[2];
+
+console.log(args);
+
+const file = fs.readFile('quotes.txt', 'utf8', (_, data) => {
+  const lines = data.split('\n');
+
+  quotes = [{"quote":"", "author":""}];
+  at = 0;
+  lines.forEach(line => {
+    if (line.trim() == '') {
+    } else if (line.startsWith("--")) {
+      quotes[at].author = line.slice(3).trim();
+      quotes[at].quote = quotes[at].quote.trim();
+      at++;
+      quotes[at] = {"quote":"", "author":""};
+    } else {
+      quotes[at].quote += line.trim().replace('\"', "'").replace('\"', "'") + ' ';
+    }
+  });
+
+  fs.writeFile(args, JSON.stringify(quotes), (_) => {});
+});
+
