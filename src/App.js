e50e2fca866b74b004ae274d911f08fcde168d85
rembrandtcosta <rembrandt.costa@ccc.ufcg.edu.br>
2023-08-22 13:54:52 -0300

    final

--- src/App.js
+++ src/App.js
@@ -29,33 +29,39 @@ function App() {
   return (
     <div className="container">
       <div id="quote-box" className="container-sm">
-        <div className="col-12 p-0 min-vh-100 text-center m-0 d-flex flex-column justify-content-center">
-          <div>
-            <div id="center-box" className="p-0 text-center m-0">
-            <p className="text-break">
-              {quote}
-            </p>
+        <div className="col col-12 p-0 min-vh-100 text-center m-0 d-flex flex-column justify-content-center">
+          <div className='row'>
+          <div className='col-lg-3'/>
+          <div className='border col-lg-6'> 
+            <div>
+              <div id="center-box" className="p-0 text-center m-0">
+              <p className="text-break mt-3">
+                {quote}
+              </p>
+              </div>
+              
+              <div id="author">
+                {"-" + author}
+              </div>
             </div>
-            
-            <div id="author">
-              {"-" + author}
+            <div className='row mt-5 mb-3'> 
+              <div className='col-sm'/>
+              <div className='col-sm'>
+                <button 
+                  id="new-quote"
+                  className='btn btn-primary' 
+                  onClick={updateRandomIndex}
+                >
+                  New Quote
+                </button>
+              </div>
+              <div className='col-sm'>
+                <a href={getTwitterURL()} id="tweet-quote"><i className='fab fa-twitter'></i> Tweet quote</a>
+              </div>
+              <div className='col-sm'/>
             </div>
           </div>
-          <div className='row'> 
-            <div className='col-sm'/>
-            <div className='col-sm'>
-              <button 
-                id="new-quote"
-                className='btn btn-primary' 
-                onClick={updateRandomIndex}
-              >
-                New Quote
-              </button>
-            </div>
-            <div className='col-sm'>
-              <a href={getTwitterURL()} id="tweet-quote">Tweet quote</a>
-            </div>
-            <div className='col-sm'/>
+          <div className='col-lg-3'/>
           </div>
         </div>
       </div>
